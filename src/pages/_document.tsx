6a03b274c6775e9a8ef6a4f316569ee4edb7dcc9
avitej-iyer <iyeravitej2@gmail.com>
2025-07-18 09:46:37 -0400

    added indexing

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html className="scroll-smooth" lang="en">
       <Head>
+        <meta name="google-site-verification" content="7ng9WEktpcbJtA1nZxhgXwIxq7V1zw70cDRUf8suU0I" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
